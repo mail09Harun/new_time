d26ab2ab7521c1090bf4ac1d358da1c09ba7b190
mail09Harun <mail09.doganhukuk@gmail.com>
2026-02-06 16:24:52 +0300

    Add files via upload

---
+++ saat-sw.js
@@ -0,0 +1,40 @@
+// Dijital Saat Service Worker
+
+const CACHE_NAME = 'dijital-saat-v1.0.0';
+const ASSETS_TO_CACHE = [
+    '/index.html',
+    '/manifest.json'
+];
+
+// Yükleme
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Aktivasyon
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
